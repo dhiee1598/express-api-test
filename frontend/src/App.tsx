78e8adda7c424bfb764af49f5871e6501fd40f46
Dexter Balderama <dhiee1598@gmail.com>
2023-07-29 15:29:01 +0800

    first commit

--- frontend/src/App.tsx
+++
@@ -1,9 +0,0 @@
-const App = () => {
-  return (
-    <div>
-      <h1 className='text-7xl'>Hello World</h1>
-    </div>
-  );
-};
-
-export default App;
--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ const url = process.env.MONGO_URL as string;
 const port = parseInt(process.env.PORT as string) || 5000;
 
 mongoose.connect(url).then(() => {
-  console.log('Mongoose Connected!');
+  console.log('Connected Successfully!');
   app.listen(port, () => {
     console.log(`Listening: http://localhost:${port}`);
   });
