5ec37930b8ca48ef5b8706a5669af51860a219e7
Dexter Balderama <dhiee1598@gmail.com>
2023-07-29 13:43:20 +0800

    applying ratelimit to goal routes

--- backend/src/api/goals/goals.routes.ts
+++ backend/src/api/goals/goals.routes.ts
@@ -1,13 +1,13 @@
 import express from 'express';
 import * as GoalHandler from './goals.handler';
-
+import * as GoalRateLimit from '../../middlewares/ratelimit';
 const router = express.Router();
 
 router.get('/', GoalHandler.getGoals);
 
 router.get('/:goalId', GoalHandler.getGoal);
 
-router.post('/', GoalHandler.createGoal);
+router.post('/', GoalRateLimit.createGoalRateLimit, GoalHandler.createGoal);
 
 router.patch('/:goalId', GoalHandler.updateGoal);
 
